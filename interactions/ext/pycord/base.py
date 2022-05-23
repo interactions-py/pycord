e037237fc9063a98c6c8baa3f9428ade31908d62
James Walston <41456914+jameswalston@users.noreply.github.com>
2022-05-23 00:16:28 -0400

    feat: build arch

---
+++ interactions/ext/pycord/base.py
@@ -0,0 +1,21 @@
+import typing
+
+from interactions import ext
+
+authors: typing.List[ext.VersionAuthor] = [
+    ext.VersionAuthor("fl0w", email="james.discord.interactions@gmail.com")
+]
+
+version = ext.Version(
+    version="0.0.1",
+    author=authors
+)
+
+base = ext.Base(
+    name="interactions-attrs",
+    version=version,
+    link="https://github.com/interactions-py/pycord",
+    description="An extension library for interactions.py to bridge with Pycord.",
+    packages=["interactions.ext.pycord"],
+    requirements=["discord-py-interactions>=4.2.0"]
+)
